7f4997a6f19e9c07caaffe218ec993400349627a
Dao Tuan Trung <v.trungdt21@vinai.io>
2025-06-08 17:48:14 +0700

    replace crystall with bongo

--- boards/shields/nice_oled/widgets/animation.c
+++ boards/shields/nice_oled/widgets/animation.c
@@ -11,23 +11,9 @@ LV_IMG_DECLARE(crystal_01);
 LV_IMG_DECLARE(crystal_02);
 LV_IMG_DECLARE(crystal_03);
 LV_IMG_DECLARE(crystal_04);
-LV_IMG_DECLARE(crystal_05);
-LV_IMG_DECLARE(crystal_06);
-LV_IMG_DECLARE(crystal_07);
-LV_IMG_DECLARE(crystal_08);
-LV_IMG_DECLARE(crystal_09);
-LV_IMG_DECLARE(crystal_10);
-LV_IMG_DECLARE(crystal_11);
-LV_IMG_DECLARE(crystal_12);
-LV_IMG_DECLARE(crystal_13);
-LV_IMG_DECLARE(crystal_14);
-LV_IMG_DECLARE(crystal_15);
-LV_IMG_DECLARE(crystal_16);
 
 const lv_img_dsc_t *crystal_imgs[] = {
-    &crystal_01, &crystal_02, &crystal_03, &crystal_04, &crystal_05, &crystal_06,
-    &crystal_07, &crystal_08, &crystal_09, &crystal_10, &crystal_11, &crystal_12,
-    &crystal_13, &crystal_14, &crystal_15, &crystal_16,
+    &crystal_01, &crystal_02, &crystal_03, &crystal_04,
 };
 
 // CONFIG_NICE_OLED_POKEMON_ANIMATION
@@ -81,7 +67,7 @@ void draw_animation(lv_obj_t *canvas, struct zmk_widget_screen *widget) {
     art = lv_animimg_create(widget->obj);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)crystal_imgs, 16);
+    lv_animimg_set_src(art, (const void **)crystal_imgs, 4);
     lv_animimg_set_duration(art, CONFIG_NICE_OLED_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
--- boards/shields/nice_oled/widgets/battery.c
+++ boards/shields/nice_oled/widgets/battery.c
@@ -11,27 +11,13 @@ LV_IMG_DECLARE(crystal_01);
 LV_IMG_DECLARE(crystal_02);
 LV_IMG_DECLARE(crystal_03);
 LV_IMG_DECLARE(crystal_04);
-LV_IMG_DECLARE(crystal_05);
-LV_IMG_DECLARE(crystal_06);
-LV_IMG_DECLARE(crystal_07);
-LV_IMG_DECLARE(crystal_08);
-LV_IMG_DECLARE(crystal_09);
-LV_IMG_DECLARE(crystal_10);
-LV_IMG_DECLARE(crystal_11);
-LV_IMG_DECLARE(crystal_12);
-LV_IMG_DECLARE(crystal_13);
-LV_IMG_DECLARE(crystal_14);
-LV_IMG_DECLARE(crystal_15);
-LV_IMG_DECLARE(crystal_16);
 
 #ifndef SET_ANIMATION_SMART_BATTERY_OFF
 #define SET_ANIMATION_SMART_BATTERY_OFF &crystal_01
 #endif
 
 const lv_img_dsc_t *crystal_imgs_test[] = {
-    &crystal_01, &crystal_02, &crystal_03, &crystal_04, &crystal_05, &crystal_06,
-    &crystal_07, &crystal_08, &crystal_09, &crystal_10, &crystal_11, &crystal_12,
-    &crystal_13, &crystal_14, &crystal_15, &crystal_16,
+    &crystal_01, &crystal_02, &crystal_03, &crystal_04,
 };
 
 static lv_obj_t *art = NULL;
@@ -45,7 +31,7 @@ static void animation_smart_battery_on(lv_obj_t *canvas) {
     art = lv_animimg_create(canvas);
     lv_obj_center(art);
 
-    lv_animimg_set_src(art, (const void **)crystal_imgs_test, 16);
+    lv_animimg_set_src(art, (const void **)crystal_imgs_test, 4);
     lv_animimg_set_duration(art, CONFIG_NICE_OLED_GEM_ANIMATION_MS);
     lv_animimg_set_repeat_count(art, LV_ANIM_REPEAT_INFINITE);
     lv_animimg_start(art);
